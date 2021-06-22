0de382840fca7c8af339884a6641ff01d4c1ce64
Nikita <62842898+NikitaKnst@users.noreply.github.com>
2021-06-22 09:44:20 +0300

    Add files via upload

--- App/Script/Search_Clean_AddStyle/SaverLastWrite.js
+++ App/Script/Search_Clean_AddStyle/SaverLastWrite.js
@@ -7,15 +7,3 @@
   chrome.storage.sync.get("AtributeSaver", ({ AtributeSaver }) => {/*Достаём из local storage значение переменной AtributeSaver*/
    document.getElementById("textAreaSecond").value=AtributeSaver/*Получаем значение переменной textSaver с Local storage и вписываем эту переменную в textarea*/  
  })/*Всё что находиться внутри этих скобок могут использовать определённую локальную переменную которую мы сохранили*/
-chrome.storage.sync.get("ColorBody", ({ ColorBody }) => {/*Достаём из local storage значение переменной AtributeSaver*/
-  function saverBody(){
-      ColorBody
-    }
-window.onload = saverBody
- })
-chrome.storage.sync.get("ColorText", ({ ColorText }) => {/*Достаём из local storage значение переменной AtributeSaver*/
-    function saverText(){
-        ColorText
-    }
-window.onload = saverText
- })
